5a9f286a46511ab08cf20a6d93b9cec46f4a9768
Piyushsingh8055 <bosspiyush98@gmail.com>
2023-06-14 18:15:05 +0530

    resume updated

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,9 +9,12 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Piyush Singh </span>
-            from <span className="purple"> Varanasi, India.</span>
-            <br />I am pursuing Master of Computer Applications in PES University,
-            Bengaluru.
+            from <span className="purple"> Bengaluru, India.</span>
+            <br />
+            <br /> 
+            I have recently completed my <span className="purple"> Master of Computer Applications </span> from  <span className="purple">PES University</span> in Bengaluru. It is a significant accomplishment, and I am proud of my achievement.
+            <br />
+            With my MCA degree, I have gained a strong foundation in various areas of computer science and applications, including programming, software development, database management, networking, and system analysis. This knowledge will be invaluable in pursuing a career in software engineering, application development, database administration, IT consulting, or research and development.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
